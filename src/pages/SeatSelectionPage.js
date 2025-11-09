40ff13ce7a111c1cbcc25ae48967eecd949d7418
hxsh01 <harsh0patil001@gmail.com>
2025-11-09 21:14:36 +0530

    added helper text

--- src/pages/SeatSelectionPage.js
+++ src/pages/SeatSelectionPage.js
@@ -13,9 +13,9 @@ export default function SeatSelectionPage({ user }) {
       <SeatGrid movieId={movieId} user={user} />
       <div className="legend">
         <span><div style={{ width: 16, height: 16, background: 'rgba(166, 231, 146, 1)', borderRadius: 4 }}></div> Available</span>
-        <span><div style={{ width: 16, height: 16, background: 'rgba(197, 128, 128, 1)', borderRadius: 4 }}></div> Reserved</span>
         <span><div style={{ width: 16, height: 16, background: '#777', borderRadius: 4 }}></div> Booked</span>
         <span><div style={{ width: 16, height: 16, background: 'rgba(10, 159, 234, 1)', borderRadius: 4 }}></div> Selected</span>
+        <span><div style={{ width: 16, height: 16, background: 'rgba(197, 128, 128, 1)', borderRadius: 4 }}></div> Reserved <span style={{ fontSize: 8 }}>* these seats might be available in some time</span></span>
       </div>
     </div>
   );
